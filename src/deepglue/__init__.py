8a512421efdcd3aab0f972d3b168fc8b6ccd7312
EricThomson <thomson.eric@gmail.com>
2024-09-22 20:38:34 -0400

    version

--- src/deepglue/__init__.py
+++ src/deepglue/__init__.py
@@ -2,7 +2,7 @@
 deepGlue: A utility package for deep learning projects.
 """
 
-__version__ = "0.1.0"  
+__version__ = "0.1.1"  
 
 # Import functions to make them available at the package level
 from .file_utils import create_subdirs
