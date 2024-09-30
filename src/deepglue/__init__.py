0f51b5a275ccd5b676ce10599547856cc8af55c9
EricThomson <thomson.eric@gmail.com>
2024-09-30 10:23:11 -0400

    plot_transformed added to plot_utils

--- src/deepglue/__init__.py
+++ src/deepglue/__init__.py
@@ -6,6 +6,7 @@ __version__ = "0.1.1"
 
 from .plot_utils import plot_category_samples
 from .plot_utils import plot_batch
+from .plot_utils import plot_transformed
 
 from .file_utils import create_subdirs
 from .file_utils import get_category_counts_by_split
@@ -14,6 +15,7 @@ from .file_utils import get_samples_per_split
 
 __all__ = ["plot_category_samples",
            "plot_batch",
+           "plot_transformed",
            "create_subdirs", 
            "get_category_counts_by_split",
            "get_samples_per_category",
--- src/deepglue/plot_utils.py
+++ src/deepglue/plot_utils.py
@@ -37,7 +37,7 @@ def plot_category_samples(data_path, category, split_type='train', num_to_plot=1
     fig : matplotlib.figure.Figure
         The figure object containing the plot.
     axes : array of matplotlib.axes
-        The array of axes objects in the plot.
+        The axes array containing the individual image subplots.
     """
     full_path = data_path / split_type / category
     
@@ -105,7 +105,7 @@ def plot_batch(batch_images, batch_targets, category_map, max_to_plot=32, cmap='
     
     fig, axes = plt.subplots(nrows=nrows, ncols=4, figsize=(7, 2*(nrows))) # size is width x height
     for index, ax in enumerate(axes.flat):
-        if index >= num_to_plot:
+        if index >= num_to_plot:  # TODO: is this really needed?
             break
         image = batch_images[index].permute(1,2,0).numpy() # originally in chan,rows,cols convert to row x col x chan
         image = rgb2gray(image)
@@ -114,3 +114,55 @@ def plot_batch(batch_images, batch_targets, category_map, max_to_plot=32, cmap='
         ax.set_title(category_map[category], fontsize=10)
         ax.axis('off')
     fig.tight_layout()
+
+
+
+def plot_transformed(original_image, transform, cmap=None, num_to_plot=4):
+    """
+    Plot the original image and pytorch transformations applied to it.
+
+    original_image : 2d array-like image
+        The original image to be transformed. Can be tensor or numpy/PIL or other array.
+    transform : pytorch transform callable
+        A transformation function (or series of transformations) to apply to the original image.
+        The function should accept an image and return a transformed tensor.
+    cmap : str, optional
+        Colormap to use for displaying greyscale images. Set to None for color images.
+    num_transforms : int, optional
+        The number of transformed images to generate and display, in addition to original image. Defaults to 4.
+
+    Returns
+    -------
+    fig : matplotlib.figure.Figure
+        The figure object containing the plots.
+    axes : array of matplotlib.axes
+        The axes array containing the individual image subplots.
+
+    Notes
+    -----
+    - The first image displayed is the original, and subsequent images are transformed versions.
+    """
+    ncols = 5
+    nrows = int(np.ceil((num_to_plot+1)/ncols))
+
+    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(8,2*nrows))
+
+    for index, ax in enumerate(axes.flat):      
+        if index >= num_to_plot+1:
+            ax.axis('off')
+            continue
+        if index == 0:
+            image = original_image
+        else:
+            image = transform(original_image).permute(1,2,0)  # permute torch tensor into shape for matplotlib
+        ax.imshow(image, cmap=cmap)
+        ax.axis('off')
+        if index == 0:
+            ax.set_title('Original')
+        else:
+            ax.set_title(f'Transform {index}')
+
+    fig.tight_layout()
+
+    return fig, axes
+            
