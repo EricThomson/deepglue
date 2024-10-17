bab1675778724e0c63e761e07f90bcacb33f81b7
EricThomson <thomson.eric@gmail.com>
2024-10-17 09:25:22 -0400

    fixed dependency bug

--- src/deepglue/__init__.py
+++ src/deepglue/__init__.py
@@ -2,7 +2,7 @@
 deepGlue: A utility package for deep learning projects.
 """
 
-__version__ = "0.1.3b"  
+__version__ = "0.1.4"  
 
 from .training_utils import train_one_epoch
 from .training_utils import validate_one_epoch
