60800a15143565f9799bb0af37dc309ddb8b1ada
EricThomson <thomson.eric@gmail.com>
2025-01-28 12:14:16 -0500

    version++

--- src/deepglue/__init__.py
+++ src/deepglue/__init__.py
@@ -2,7 +2,7 @@
 deep glue: A utility package for creating deep learning projects with pytorch.
 """
 
-__version__ = "0.1.11"  
+__version__ = "0.1.12"  
 
 from .training_utils import train_one_epoch
 from .training_utils import validate_one_epoch
